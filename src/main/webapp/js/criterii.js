eb6e18b631b86fa5d9988f711d067a38060e3366
Beatrice Luca <bianca.luca@mindit.ro>
2017-04-09 18:29:41 +0300

    Pret min/max javascript

--- src/main/webapp/js/criterii.js
+++ src/main/webapp/js/criterii.js
@@ -1,3 +1,26 @@
-function afiseazaPret(val) {
-	document.getElementById("valoare").innerHTML = " < " + val;
-}
+window.onload = function(){
+	document.getElementById("criterii").onclick = function() {
+		if(document.getElementById("pret-max").value != ""){
+			if(document.getElementById("pret-min").value > document.getElementById("pret-max").value)
+				alert("Ai completat gresit pretul maxim!");
+		}
+	};
+	
+	document.getElementById("pret-min").onkeyup = function(e) {
+		if(e.keyCode < 48 || e.keyCode > 57) {
+			if(e.keyCode != 13) {
+				document.getElementById("pret-min").value="";
+			}
+		}
+	};
+	
+	document.getElementById("pret-max").onkeyup = function(e) {
+		if(e.keyCode < 48 || e.keyCode > 57) {
+			if(e.keyCode != 13) {
+				document.getElementById("pret-max").value="";
+			}
+		}
+	};
+};
+
+
