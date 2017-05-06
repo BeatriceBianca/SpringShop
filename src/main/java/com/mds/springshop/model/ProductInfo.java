68708976b3880618236f888915fb4a646c467f6c
Beatrice Luca <bianca.luca@mindit.ro>
2017-05-06 16:38:36 +0300

    changes

--- src/main/java/com/mds/springshop/model/ProductInfo.java
+++ src/main/java/com/mds/springshop/model/ProductInfo.java
@@ -10,7 +10,7 @@ public class ProductInfo {
 	private int productsLeftInStock;
 	private long price;
 	private int status;
-	private int category;
+	private Categories category;
   private int id;
 	private long minPrice;
 	private long maxPrice;
@@ -45,7 +45,7 @@ public class ProductInfo {
         this.id = product.getId();
     }
  
-    public ProductInfo(String name, int productsLeftInStock, long price, int status, int category, int id) {
+    public ProductInfo(String name, int productsLeftInStock, int price, int status, Categories category, int id) {
         this.name = name;
         this.productsLeftInStock = productsLeftInStock;
         this.price = price;
@@ -86,11 +86,11 @@ public class ProductInfo {
 		this.status = status;
 	}
 	
-	public int getCategory() {
+	public Categories getCategory() {
 		return category;
 	}
 
-	public void setCategory(int category) {
+	public void setCategory(Categories category) {
 		this.category = category;
 	}
 
